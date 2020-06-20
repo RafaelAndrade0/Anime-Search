6fdf6ced16cda03106edc3d4380f75a79944f718
Rafael <rafael.andrade.dev0@gmail.com>
2020-06-20 10:41:53 -0300

    Utilizando retorno correto no left de IAuthFacade

--- lib/domain/auth/i_auth_facade.dart
+++ lib/domain/auth/i_auth_facade.dart
@@ -1,18 +1,19 @@
+import 'package:anime_search/domain/auth/auth_failure.dart';
 import 'package:anime_search/domain/auth/value_objects.dart';
 import 'package:anime_search/domain/core/failures.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 
 abstract class IAuthFacade {
-  Future<Either<ValueFailure<String>, Unit>> registerWithEmailAndPassword({
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
     @required EmailAddress emailAddress,
     @required Password password,
   });
 
-  Future<Either<ValueFailure<String>, Unit>> signInWithEmailAndPassword({
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
     @required EmailAddress emailAddress,
     @required Password password,
   });
 
-  Future<Either<ValueFailure<String>, Unit>> signInWithGoogle();
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
 }
