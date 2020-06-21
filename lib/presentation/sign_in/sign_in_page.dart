24819d32542c4446cd6317aaecd9540353d02b4f
Rafael <rafael.andrade.dev0@gmail.com>
2020-06-21 18:03:54 -0300

    Retirando appbar da SignInPage

--- lib/presentation/sign_in/sign_in_page.dart
+++ lib/presentation/sign_in/sign_in_page.dart
@@ -8,9 +8,6 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign In'),
-      ),
       body: BlocProvider(
         create: (context) => getIt<SignInFormBloc>(),
         child: SignInForm(),
