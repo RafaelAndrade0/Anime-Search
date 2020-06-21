1253b210d5a2a62bdca46fe02ca68e49300f2785
Rafael <rafael.andrade.dev0@gmail.com>
2020-06-21 17:14:42 -0300

    Incluindo loader

--- lib/presentation/sign_in/widgets/sign_in_form.dart
+++ lib/presentation/sign_in/widgets/sign_in_form.dart
@@ -1,4 +1,5 @@
 import 'package:anime_search/application/auth/sign_in_form/sign_in_form_bloc.dart';
+import 'package:anime_search/presentation/loader/loader.dart';
 import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
@@ -35,101 +36,109 @@ class SignInForm extends StatelessWidget {
         );
       },
       builder: (context, state) {
-        return Form(
-          autovalidate: state.showErrorMessages,
-          child: ListView(
-            padding: const EdgeInsets.all(20),
-            children: <Widget>[
-              Container(
-                height: 320,
-                child: Image.asset(
-                  'assets/images/logo.png',
-                  fit: BoxFit.fitHeight,
-                ),
-              ),
-              TextFormField(
-                autocorrect: false,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.email),
-                  labelText: 'Email',
-                ),
-                onChanged: (value) => context
-                    .bloc<SignInFormBloc>()
-                    .add(SignInFormEvent.emailChanged(value)),
-                validator: (_) => context
-                    .bloc<SignInFormBloc>()
-                    .state
-                    .emailAddress
-                    .value
-                    .fold(
-                        (l) => l.maybeMap(
-                              invalidEmail: (_) => 'Invalid Email',
-                              orElse: () => null,
-                            ),
-                        (_) => null),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              TextFormField(
-                autocorrect: false,
-                obscureText: true,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.lock),
-                  labelText: 'Password',
-                ),
-                onChanged: (value) => context
-                    .bloc<SignInFormBloc>()
-                    .add(SignInFormEvent.passwordChanged(value)),
-                validator: (_) =>
-                    context.bloc<SignInFormBloc>().state.password.value.fold(
-                        (l) => l.maybeMap(
-                              shortPassword: (_) => 'Short Password',
-                              orElse: () => null,
-                            ),
-                        (_) => null),
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: OutlineButton(
-                      onPressed: () {
-                        context.bloc<SignInFormBloc>().add(const SignInFormEvent
-                            .signInWithEmailAndPasswordPressed());
-                      },
-                      child: const Text('LOG IN'),
-                    ),
-                  ),
-                  Expanded(
-                    child: OutlineButton(
-                      borderSide: BorderSide(
-                        width: 2,
-                        color: Colors.deepPurple,
+        return state.isSubmitting
+            ? Loader()
+            : Form(
+                autovalidate: state.showErrorMessages,
+                child: ListView(
+                  padding: const EdgeInsets.all(20),
+                  children: <Widget>[
+                    Container(
+                      height: 320,
+                      child: Image.asset(
+                        'assets/images/logo.png',
+                        fit: BoxFit.fitHeight,
                       ),
-                      onPressed: () {
-                        context.bloc<SignInFormBloc>().add(const SignInFormEvent
-                            .registerWithEmailAndPasswordPressed());
-                      },
-                      child: const Text('CREATE ACCOUNT'),
                     ),
-                  )
-                ],
-              ),
-              GoogleSignInButton(
-                text: 'Log in with google',
-                onPressed: () {},
-                darkMode: true,
-              ),
-              const Text(
-                'By Using Anime Search you are agreeing to our terms & Privacy Policy. And you confirm that you are at least 18 years or age.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 12,
+                    TextFormField(
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.email),
+                        labelText: 'Email',
+                      ),
+                      onChanged: (value) => context
+                          .bloc<SignInFormBloc>()
+                          .add(SignInFormEvent.emailChanged(value)),
+                      validator: (_) => context
+                          .bloc<SignInFormBloc>()
+                          .state
+                          .emailAddress
+                          .value
+                          .fold(
+                              (l) => l.maybeMap(
+                                    invalidEmail: (_) => 'Invalid Email',
+                                    orElse: () => null,
+                                  ),
+                              (_) => null),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextFormField(
+                      autocorrect: false,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock),
+                        labelText: 'Password',
+                      ),
+                      onChanged: (value) => context
+                          .bloc<SignInFormBloc>()
+                          .add(SignInFormEvent.passwordChanged(value)),
+                      validator: (_) => context
+                          .bloc<SignInFormBloc>()
+                          .state
+                          .password
+                          .value
+                          .fold(
+                              (l) => l.maybeMap(
+                                    shortPassword: (_) => 'Short Password',
+                                    orElse: () => null,
+                                  ),
+                              (_) => null),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: OutlineButton(
+                            onPressed: () {
+                              context.bloc<SignInFormBloc>().add(
+                                  const SignInFormEvent
+                                      .signInWithEmailAndPasswordPressed());
+                            },
+                            child: const Text('LOG IN'),
+                          ),
+                        ),
+                        Expanded(
+                          child: OutlineButton(
+                            borderSide: BorderSide(
+                              width: 2,
+                              color: Colors.deepPurple,
+                            ),
+                            onPressed: () {
+                              context.bloc<SignInFormBloc>().add(
+                                  const SignInFormEvent
+                                      .registerWithEmailAndPasswordPressed());
+                            },
+                            child: const Text('CREATE ACCOUNT'),
+                          ),
+                        )
+                      ],
+                    ),
+                    GoogleSignInButton(
+                      text: 'Log in with google',
+                      onPressed: () {},
+                      darkMode: true,
+                    ),
+                    const Text(
+                      'By Using Anime Search you are agreeing to our terms & Privacy Policy. And you confirm that you are at least 18 years or age.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
-          ),
-        );
+              );
       },
     );
   }
